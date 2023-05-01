fa6cf14623f3333c9867a5b8675d82928c526317
Varshitha <varshithadas05@gmail.com>
2023-05-01 13:35:33 +0530

    comment

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,71 +1,81 @@
 import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const customer = await prisma.customer.create({
-    data: {
-      customer_name: "Amulya",
-      customer_email: 'amulya098@gmail.com',
-      phone_number: 9123456787,
-      address: 'KR puram,Karnataka',
-      password: 'amulyasa098',
-      age: 19,
-    },
-  });
+  await prisma.request.deleteMany();
+  await prisma.review.deleteMany();
+  await prisma.history.deleteMany();
+  await prisma.service.deleteMany();
+  await prisma.serviceprovider.deleteMany();
+  await prisma.customer.deleteMany();
 
-  const serviceProvider = await prisma.serviceprovider.create({
-    data: {
-      serviceprovider_name: 'Arlo',
-      serviceprovider_email: 'alro123@gmail.com',
-      phone_number: 8765432198,
-      gstregistration_number: 'GST878765',
-      chargers_for_particular_service: 890,
-      specialisation: 'Deliverying food',
-      service_name: 'Food delivery',
-      age: 29,
-      address: 'Bngalore',
-      password: 'evaovan1234',
-    },
-  });
+  for (let i = 0; i < 10; i++) {
+    const customer = await prisma.customer.create({
+      data: {
+        customer_name: faker.name.firstName(),
+        customer_email: faker.internet.email(),
+        phone_number: BigInt(faker.phone.number('+91#########')),
+        address: faker.address.streetAddress(),
+        password: faker.internet.password(),
+        age: faker.datatype.number({ min: 18, max: 80 }),
+      },
+    });
 
-  const service = await prisma.service.create({
-    data: {
-      service_name: 'Food delivery',
-      serviceprovider_id: serviceProvider.serviceprovider_id,
-    },
-  });
+    const serviceProvider = await prisma.serviceprovider.create({
+      data: {
+        serviceprovider_name: faker.company.companyName(),
+        serviceprovider_email: faker.internet.email(),
+        phone_number: BigInt(faker.phone.number('91#########')),
+        gstregistration_number: faker.random.alphaNumeric(8),
+        chargers_for_particular_service: faker.datatype.number({ min: 50, max: 500 }),
+        specialisation: faker.lorem.word(),
+        service_name: faker.lorem.word(),
+        age: faker.datatype.number({ min: 18, max: 80 }),
+        address: faker.address.streetAddress(),
+        password: faker.internet.password(),
+      },
+    });
 
-  const history = await prisma.history.create({
-    data: {
-      date: new Date(),
-      customer_id: customer.customer_id,
-      serviceprovider_id: serviceProvider.serviceprovider_id,
-      service_id: service.service_id,
-    },
-  });
+    const service = await prisma.service.create({
+      data: {
+        service_name: faker.lorem.word(),
+        serviceprovider_id: serviceProvider.serviceprovider_id,
+      },
+    });
 
-  const review = await prisma.review.create({
-    data: {
-      customer_id: customer.customer_id,
-      service_id: service.service_id,
-      rating: 4.5,
-      review_content: 'Very tasty food !',
-    },
-  });
+    const history = await prisma.history.create({
+      data: {
+        date: faker.date.recent(),
+        customer_id: customer.customer_id,
+        serviceprovider_id: serviceProvider.serviceprovider_id,
+        service_id: service.service_id,
+      },
+    });
 
-  const request = await prisma.request.create({
-    data: {
-      reasons_for_rejecting: "service not available on that date",
-      date: new Date(),
-      time: new Date(),
-      serviceprovider_id: serviceProvider.serviceprovider_id,
-      customer_id: customer.customer_id,
-      service_id: service.service_id,
-    },
-  });
+    const review = await prisma.review.create({
+      data: {
+        customer_id: customer.customer_id,
+        service_id: service.service_id,
+        rating: faker.datatype.float({ min: 1, max: 5, precision: 0.1 }),
+        review_content: faker.lorem.sentences(),
+      },
+    });
 
-  console.log('One record seeded for each model!');
+    const request = await prisma.request.create({
+      data: {
+        reasons_for_rejecting: faker.lorem.sentence(),
+        date: faker.date.future(),
+        time: faker.date.future(),
+        serviceprovider_id: serviceProvider.serviceprovider_id,
+        customer_id: customer.customer_id,
+        service_id: service.service_id,
+      },
+    });
+  }
+
+  console.log('Data seeded successfully!');
 }
 
 main()
