8fbb35b83f1c1d9c05ca3bf5cac0c9cfc2275149
Varshitha <varshithadas05@gmail.com>
2023-05-03 16:22:47 +0530

    add nextjs, tailwind css

--- src/pages/api/about.tsx
+++ src/pages/api/about.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image'
+
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
 export default function About() {
     return (
       <main
