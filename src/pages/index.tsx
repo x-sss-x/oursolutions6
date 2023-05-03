e0687c2bef857ecdb6bc23005feeca12241c8ad9
Varshitha <varshithadas05@gmail.com>
2023-05-03 15:57:33 +0530

    add  nextjs, tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-    <ul></ul><h1 className='text-xl font-bold font-sans'>OurSolutions<br/>
+   <h1 className='text-xl font-bold font-sans'>OurSolutions
     (multiservices)
-    </h1><ul/>
+    </h1>
     </main>
   )
 }
