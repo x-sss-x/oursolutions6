f6749f5dde988c78e4aac1c237b1d1344135018d
Varshitha <varshithadas05@gmail.com>
2023-06-16 12:05:12 +0530

    adding changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e5309852cca4910"; }
+/******/ 		__webpack_require__.h = function() { return "01bdfe0be73a7f8a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
