cfd21c970a18bfefc4d4cadc652e6a20d6ba22f5
Manoj <66366064+x-sss-x@users.noreply.github.com>
2023-06-23 18:29:09 +0000

    ffff

--- hooks.ts
+++
@@ -1,5 +0,0 @@
-import {useDispatch} from "react-redux"
-import { AppDispatch } from "./src/store";
-
-type DispatchFunc = () => AppDispatch;
-export const useAppDispatch: DispatchFunc = useDispatch;
